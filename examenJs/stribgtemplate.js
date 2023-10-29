8aa31e9a47bef1fa1b0bf4ad47d2eebba76a3075
Gustavo Gamarra <gugaro8@gmail.com>
2023-10-29 20:48:59 -0300

    respuesta actualizada

---
+++ examenJs/stribgtemplate.js
@@ -0,0 +1,5 @@
+const  presentacion = (nombre, edad) => `Hola, mi nombre  ${nombre} y tengo ${edad} anos.`;
+const nombre = "Gustavo";
+const edad = 28;
+const saludo = presentacion(nombre, edad);
+console.log(saludo);
