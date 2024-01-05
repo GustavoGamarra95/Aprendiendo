86c4d8fbd298249959a99112f788024cd36a7958
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-05 16:16:21 -0300

    Tipo de dato booleano

---
+++ Python/aula5.py
@@ -0,0 +1,12 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis:
+# sim (True) ou não (False).
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que
+# questiona se um valor é igual a outro
+print(10 == 10) # Sim => True (Verdadero)
+print(10 == 11) # Falso => False (Falso)
+print (type(10==10)) #class bool
+print (type(11==10)) #class bool
+
