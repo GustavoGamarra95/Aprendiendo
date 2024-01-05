27b7e4cac25a6c40269e066bf1916dc1d1bc8874
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-05 15:08:24 -0300

    Tipos de DocStrings y comentarios

---
+++ Python/aula2.py
@@ -0,0 +1,12 @@
+''' DocStrings '''
+"""
+Apartado de comentarios
+
+se puede escrbir salteando las lineas
+""" 
+
+'''
+Tambien se puede usar como apartado de comentarios 
+funciona como las tres comillas 
+'''
+#Comentario
