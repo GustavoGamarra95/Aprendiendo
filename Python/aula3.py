cab975337c673019652bf1a357c0bec69b024b80
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-05 15:41:13 -0300

    Strings y Comillas dobles y sencillas

---
+++ Python/aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Lenguaje de Programacion
+Tipo de tipage =  Dinamica / Fuerte
+str -> string -> texto
+Strings son textos que estan dentro de las comillas
+"""
+print(1234), #los numeros no necesitan de estar entre comillas para poder incluir dentro de las lineas
+print('Hola, Mundo'), #comillas simples
+print(1,'Hola, "Mundo"'),
+print("Hola, Mundo"); #comillas duplas
+print(2,"Hola, 'Mundo'"),
+
+#en ambos casos las letras son usadas como strings pero pueden sufrir de variaciones.
+
+
+# escape
+print("Hola, \"Mundo\" ");
+
+#r
+print(r"Hola, \"Mundo\"");
+
