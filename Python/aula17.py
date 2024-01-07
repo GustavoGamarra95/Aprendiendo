0977743ab9b04fc2b7e3cd3f432225c215be474d
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-07 20:14:04 -0300

    if, elif, else

---
+++ Python/aula17.py
@@ -0,0 +1,23 @@
+# if / elif     / else (else y elif dependen de if)
+# si / si no si / si no
+
+condicion1 =  False
+condicion2 =  False
+condicion3 =  True
+condicion4 =  False
+
+if condicion1:
+    print('Codigo para condicion1')
+elif condicion2:
+    print('Codigo para condicion2')
+elif condicion3:
+    print('Codigo para condicion3')
+elif condicion4:
+    print('Codigo para condicion4')
+else:
+    print('Ninguna condicion fue cumplida')
+
+if 10 == 10:
+    print('Otro if')
+
+print('fuera del if')
