2bdc3f37dd5e0736bd8b7014a48a390e36560f6a
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-07 17:15:56 -0300

    ejercicio de calculo de imc

---
+++ Python/aula12.py
@@ -0,0 +1,10 @@
+#ejercicio de calculo de IMC
+
+nombre = 'Gustavo'
+altura = 1.90
+peso = 135
+imc = ...
+calculo_imc = peso / (altura**2)
+
+print('Gustavo tiene una altura de', altura)
+print('Posee un IMC de', calculo_imc)
