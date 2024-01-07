9908e40a5b56662bccf0497b9b509a0f4606c32d
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-07 16:04:46 -0300

    formas de utilizar variables

---
+++ Python/aula7.py
@@ -0,0 +1,19 @@
+"""
+variables son utilizadas para guardar algo en la memoria del computador.
+PEP8: inicia variables con letras minusculas, puede usar 
+numeros y guin bajo _.
+La senal = es el operador de atribuicion, es usado para 
+atribuir un valor a un nombre (variable).
+nombre_variable = expresion.
+"""
+nombre_completo = 'Gustavo Gamarra'
+suma_dos_mas_dos = 2+2
+int_uno = bool('1')
+print(nombre_completo, suma_dos_mas_dos)
+
+
+nombre_1 = 'Gustavo'
+edad = 28
+mayor_de_edad = edad >= 18
+print('nombre_1', nombre_1, 'edad', edad)
+print('Es mayor de edad?', mayor_de_edad)
