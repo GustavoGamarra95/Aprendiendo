51c91399272baa7de2885ca06ce5a428a465fb5a
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-07 20:16:00 -0300

    iif / elif/ else parte 1

---
+++ Python/aula16.py
@@ -0,0 +1,12 @@
+# if / elif     / else
+# si / si no si / si no
+
+entrada = input('Deseas entrar "entar" o "salir"? ')
+
+if entrada == 'entrar':
+    print('Entraste al sistema')
+elif entrada == 'salir':
+    print('Saliste del sistema')
+else:
+    print('Nose digito entar o salir')
+    
