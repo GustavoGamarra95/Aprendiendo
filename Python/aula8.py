2eaccf849c63a9ed7a7661973d342c1a48e8cd0d
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-07 16:05:32 -0300

    ejercicios de los terminos ya aprendidos

---
+++ Python/aula8.py
@@ -0,0 +1,13 @@
+Nombre = 'Gustavo'
+Apellido = 'Gamarra'
+Edad = 29
+ano_nacimento = 2024 - Edad
+mayor_de_edad = Edad >= 18
+altura_metros = 1.90
+
+print('Nombre:', Nombre)
+print('Apellido:', Apellido)
+print('Edad:', Edad)
+print('Ano de nacimento:', ano_nacimento)
+print('Es mayor de edad?', mayor_de_edad)
+print('Altura en metros:', altura_metros)
