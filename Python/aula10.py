7f1271ca70e93faa2655406a24281aec80533cf0
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-07 17:14:23 -0300

    concatenizacion

---
+++ Python/aula10.py
@@ -0,0 +1,10 @@
+#concatenizacion
+concatenicacion = 'Gustavo' + '' + 'Gamarra'
+print( concatenicacion)
+
+concatenicacion = 'Gustavo' + str(1) + ''
+print( concatenicacion)
+
+a_dies_veces = 'A' * 10
+tres_veces_gustavo = 3 * 'Gustavo'
+print(a_dies_veces, tres_veces_gustavo)
