2ac715e9e939c79ccbd56cd167f4ab30fa9d1d52
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-15 21:29:17 -0300

    Fatiamento de strings

---
+++ Python/aula27.py
@@ -0,0 +1,22 @@
+"""
+Fatiamento de strings
+0123456789
+Hola mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs: la funcion len retorna la cantidad
+de caracteres en str
+
+"""
+variable = 'Hola mundo'
+print(variable[1])
+print(variable[3])
+print(variable[:3])
+print(len(variable))
+print(variable[0:len(variable):1])
+print(variable[0:9:3])
+print(variable[::-1])
+
+
+
+
