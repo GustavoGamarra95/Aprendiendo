2b9f1a78d3e7022b3581039778d5f29247fb1bb8
Gustavo Gamarra <gugaro8@gmail.com>
2024-01-15 21:18:04 -0300

    formatos de strings

--- Python/aula26.py
+++ Python/aula26.py
@@ -14,3 +14,16 @@ Sinal - + ou -
 Ex.: 0>-100,.1f
 Conversion flags - !r !s !a 
 """
+variable = 'abc'
+print(f'{variable}')
+print(f'{variable: >10}')
+print(f'{variable: <10}')
+print(f'{variable: ^10}')
+print (f'{1000.4875645:0>+10,.1f}')
+print(f'El hexadecimal de 1500 es {1500:08x}')
+print(f'{variable!r}')
+
+
+
+
+
